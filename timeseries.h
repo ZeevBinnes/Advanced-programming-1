d5bd88ff879a4b8d4a13678c5d165b46a0a0778a
ZeevBinnes <92650073+ZeevBinnes@users.noreply.github.com>
2021-11-14 17:52:58 +0200

    added requiered functions to timeseries

--- timeseries.h
+++ timeseries.h
@@ -3,6 +3,8 @@
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
 
+#include <string>
+
 using namespace std;
 
 class TimeSeries{
@@ -11,6 +13,11 @@ public:
 
 	TimeSeries(const char* CSVfileName){
 	}
+
+	float* getColumn(int index) const;
+	string getHeadLine(int index) const;	// get the features name
+	int getSize() const;	// get the number of columns
+	int getLength() const;	// get the number of rows
 
 };
 
